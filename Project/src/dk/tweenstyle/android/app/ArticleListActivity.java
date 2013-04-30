032d48d9766172faf13920d7e280bbb9a42874a6
Viktor <viktors.nikolov@gmail.com>
2013-04-30 11:03:19 +0200

    Le wild Gender enum.
    Signed-off-by: Viktor <viktors.nikolov@gmail.com>

---
+++ Project/src/dk/tweenstyle/android/app/ArticleListActivity.java
@@ -0,0 +1,9 @@
+package dk.tweenstyle.android.app;
+
+import android.app.Activity;
+
+public class ArticleListActivity extends Activity{
+	
+	
+
+}
--- Project/src/dk/tweenstyle/android/app/model/Gender.java
+++ Project/src/dk/tweenstyle/android/app/model/Gender.java
@@ -2,4 +2,26 @@ package dk.tweenstyle.android.app.model;
 
 public enum Gender {
 	
+	/**
+	 * Enumeration class for gender categorization of articles
+	 * @author Viktor
+	 */
+	
+//	MALE("Boys",0),
+//	FEMALE("Girls",1),
+//	UNISEX("B",2);
+//	
+//	private String stringValue;
+//	private int intValue;
+//	private Gender(String toString,int value){
+//		stringValue=toString;
+//		intValue=value;
+//	}
+//	
+//	@Override
+//	public String toString(){
+//		return stringValue;
+//	}
+//	
+MALE,FEMALE,UNSIEX;	
 }
