be9143cb6c2baa7bbf4299b69c96c069c7cbb282
Ellmor <susu@o2.pl>
2013-05-01 11:02:34 +0200

    MainJSONLoader.java and MemoryDAO.java done, also fragment added in activity_categories.xml
    Signed-off-by: Ellmor <susu@o2.pl>

--- Project/src/dk/tweenstyle/android/app/dao/MainJSONLoader.java
+++ Project/src/dk/tweenstyle/android/app/dao/MainJSONLoader.java
@@ -1,39 +1,92 @@
 package dk.tweenstyle.android.app.dao;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.util.Log;
 import dk.tweenstyle.android.app.model.Discount;
 import dk.tweenstyle.android.app.model.Group;
 import dk.tweenstyle.android.app.model.Product;
 import dk.tweenstyle.android.app.model.Settings;
-import android.net.Uri;
 
 public class MainJSONLoader {
 	private JSONLoader<Product> ldrProduct;
 	private JSONLoader<Discount> ldrDiscount;
 	private JSONLoader<Group> ldrGroup;
 	private JSONLoader<Settings> ldrSettings;
-	
+
 	public MainJSONLoader() {
 		this.setupLoaders();
 	}
-	
+
 	private void setupLoaders() {
 		this.ldrProduct = new ProductJSONLoader();
 		this.ldrDiscount = new DiscountJSONLoader();
 		this.ldrGroup = new GroupJSONLoader();
 		this.ldrSettings = new SettingsJSONLoader();
 	}
-	
-	public String fetchJSONData(Uri uri) {
-		// TODO Implement this!!! Get a JSON String from the remote server.
-		return null;
+
+	public String fetchJSONData(URI uri) {
+		String result = null;
+		try {
+			DefaultHttpClient httpclient = new DefaultHttpClient(new BasicHttpParams());
+			HttpPost httppost = new HttpPost(uri);
+			httppost.setHeader("Content-type", "application/json");
+
+			InputStream inputStream = null;
+			HttpResponse response = httpclient.execute(httppost);
+			HttpEntity entity = response.getEntity();
+
+			inputStream = entity.getContent();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"), 8);
+			StringBuilder sb = new StringBuilder();
+
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				sb.append(line + "\n");
+			}
+			result = sb.toString();
+		} catch (Exception e) {
+			Log.e("json", "Error while fetching data from CMS:" + e);
+		}
+		return result;
 	}
-	
+
 	public MemoryDAO loadJSONData(String data) {
 		MemoryDAO dao = new MemoryDAO();
-		
-		// TODO Implement this!!! Call the loaders and put the data into the
-		// dao.
-		
+		try {
+			JSONObject jObject = new JSONObject(data);
+			JSONArray products = jObject.getJSONArray("products");
+			for (int i = 0; i < products.length(); i++) {
+				JSONObject oneObject = products.getJSONObject(i);
+				dao.addProduct(ldrProduct.loadObject(oneObject));
+			}
+			JSONArray groups = jObject.getJSONArray("groups");
+			for (int i = 0; i < groups.length(); i++) {
+				JSONObject oneObject = groups.getJSONObject(i);
+				dao.addGroup(ldrGroup.loadObject(oneObject));
+			}
+			JSONArray discounts = jObject.getJSONArray("discounts");
+			for (int i = 0; i < discounts.length(); i++) {
+				JSONObject oneObject = discounts.getJSONObject(i);
+				dao.addDiscount(ldrDiscount.loadObject(oneObject));
+			}
+			JSONObject settings = jObject.getJSONObject("settings");
+			dao.setSettings(ldrSettings.loadObject(settings));
+		} catch (JSONException e) {
+			Log.e("json", "Error while fetching data from JSON:" + e);
+		}
 		return dao;
 	}
 }
--- Project/src/dk/tweenstyle/android/app/dao/MemoryDAO.java
+++ Project/src/dk/tweenstyle/android/app/dao/MemoryDAO.java
@@ -1,5 +1,52 @@
 package dk.tweenstyle.android.app.dao;
 
+import java.util.ArrayList;
+
+import dk.tweenstyle.android.app.model.Discount;
+import dk.tweenstyle.android.app.model.Group;
+import dk.tweenstyle.android.app.model.Product;
+import dk.tweenstyle.android.app.model.Settings;
+
 public class MemoryDAO {
-	
+
+	private ArrayList<Product> products = new ArrayList<Product>();
+	private ArrayList<Group> groups = new ArrayList<Group>();
+	private ArrayList<Discount> discounts = new ArrayList<Discount>();
+	private Settings settings = null;
+
+	public Product addProduct(Product product) {
+		products.add(product);
+		return product;
+	}
+
+	public boolean removeProduct(Product product) {
+		return products.remove(product);
+	}
+
+	public Group addGroup(Group group) {
+		groups.add(group);
+		return group;
+	}
+
+	public boolean removeGroup(Group group) {
+		return groups.remove(group);
+	}
+
+	public Discount addDiscount(Discount discount) {
+		discounts.add(discount);
+		return discount;
+	}
+
+	public boolean removeDiscount(Discount discount) {
+		return discounts.remove(discount);
+	}
+
+	public Settings getSettings() {
+		return settings;
+	}
+
+	public void setSettings(Settings settings) {
+		this.settings = settings;
+	}
+
 }
