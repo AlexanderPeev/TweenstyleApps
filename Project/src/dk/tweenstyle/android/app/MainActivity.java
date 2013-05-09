a66552dafe82ffeeb6e17356f3bacc438c054e2e
Ellmor <susu@o2.pl>
2013-05-09 17:55:53 +0200

    LOL
    Signed-off-by: Ellmor <susu@o2.pl>

--- Project/src/dk/tweenstyle/android/app/MainActivity.java
+++ Project/src/dk/tweenstyle/android/app/MainActivity.java
@@ -21,7 +21,7 @@ import dk.tweenstyle.android.app.model.Settings;
  * 
  */
 public class MainActivity extends FragmentActivity {
-	// thomas
+	// Thomas and Piotr
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -30,67 +30,55 @@ public class MainActivity extends FragmentActivity {
 		btnBoys.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				Intent i = new Intent(MainActivity.this,
-						CategoriesActivity.class);
+				Intent i = new Intent(MainActivity.this, CategoriesActivity.class);
 				MemoryDAO dao = MemoryDAO.getInstance();
 				if (dao != null) {
 					Settings settings = dao.getSettings();
 					if (settings != null) {
-						String groupID = settings
-								.getValue(Settings.SETTINGS_KEY_BOYS_GROUP_ID);
+						String groupID = settings.getValue(Settings.SETTINGS_KEY_BOYS_GROUP_ID);
 						Log.d("groups", "Sending groupID == " + groupID);
 						if (groupID != null) {
-							i.putExtra(
-									CategoriesActivity.INTENT_EXTRA_KEY_GROUP_ID,
-									groupID);
+							i.putExtra(CategoriesActivity.INTENT_EXTRA_KEY_GROUP_ID, groupID);
 						}
 					}
 				}
 				startActivity(i);
 			}
 		});
-		
+
 		Button btnGirls = (Button) findViewById(R.id.btnGirls);
 		btnGirls.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				Intent i = new Intent(MainActivity.this,
-						CategoriesActivity.class);
+				Intent i = new Intent(MainActivity.this, CategoriesActivity.class);
 				MemoryDAO dao = MemoryDAO.getInstance();
 				if (dao != null) {
 					Settings settings = dao.getSettings();
 					if (settings != null) {
-						String groupID = settings
-								.getValue(Settings.SETTINGS_KEY_GIRLS_GROUP_ID);
+						String groupID = settings.getValue(Settings.SETTINGS_KEY_GIRLS_GROUP_ID);
 						Log.d("groups", "Sending groupID == " + groupID);
 						if (groupID != null) {
-							i.putExtra(
-									CategoriesActivity.INTENT_EXTRA_KEY_GROUP_ID,
-									groupID);
+							i.putExtra(CategoriesActivity.INTENT_EXTRA_KEY_GROUP_ID, groupID);
 						}
 					}
 				}
 				startActivity(i);
 			}
 		});
-		
+
 		Button btnTweens = (Button) findViewById(R.id.btnTweens);
 		btnTweens.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				Intent i = new Intent(MainActivity.this,
-						CategoriesActivity.class);
+				Intent i = new Intent(MainActivity.this, CategoriesActivity.class);
 				MemoryDAO dao = MemoryDAO.getInstance();
 				if (dao != null) {
 					Settings settings = dao.getSettings();
 					if (settings != null) {
-						String groupID = settings
-								.getValue(Settings.SETTINGS_KEY_BRANDS_GROUP_ID);
+						String groupID = settings.getValue(Settings.SETTINGS_KEY_BRANDS_GROUP_ID);
 						Log.d("groups", "Sending groupID == " + groupID);
 						if (groupID != null) {
-							i.putExtra(
-									CategoriesActivity.INTENT_EXTRA_KEY_GROUP_ID,
-									groupID);
+							i.putExtra(CategoriesActivity.INTENT_EXTRA_KEY_GROUP_ID, groupID);
 						}
 					}
 				}
@@ -99,7 +87,7 @@ public class MainActivity extends FragmentActivity {
 		});
 		initMainJSONLoader();
 	}
-	
+
 	private void initMainJSONLoader() {
 		final ProgressDialog pd = ProgressDialog.show(this, "Please wait", "");
 		Log.d("json", "Starting init... ");
@@ -109,9 +97,8 @@ public class MainActivity extends FragmentActivity {
 				MainJSONLoader jl = new MainJSONLoader();
 				try {
 					Log.d("json", "Starting to load... ");
-					
-					String jsonData = jl.fetchJSONData(new URI(
-									"http://tweenstylekopi.net.dynamicweb.dk/Default.aspx?ID=3725"));
+
+					String jsonData = jl.fetchJSONData(new URI("http://tweenstylekopi.net.dynamicweb.dk/Default.aspx?ID=3725"));
 
 					Log.d("json", "Done fetching... ");
 					MemoryDAO dao = jl.loadJSONData(jsonData);
@@ -121,25 +108,24 @@ public class MainActivity extends FragmentActivity {
 					Log.d("json", "Done hooking up groups... ");
 					dao.hookupProducts();
 					Log.d("json", "Done hooking up products... ");
-					
+
 					Log.d("json", "Total products: " + dao.getTotalProducts());
-					
-				}
-				catch (URISyntaxException e) {
+
+				} catch (URISyntaxException e) {
 					Log.d("json", "Malformed uri: ", e);
 				}
 				pd.dismiss();
 			}
 		});
-		
+
 		t.start();
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
+
 }
